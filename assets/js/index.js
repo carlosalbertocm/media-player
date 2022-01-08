97021de991847e013fb9cb307accbda097e76b00
Carlos Alberto Conde Moreno <cacm1977@gmail.com>
2022-01-07 19:17:11 -0500

    Scope, module

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,7 @@
+import MediaPlayer from "./MediaPlayer.js";
+
 const video = document.querySelector("video");
-const button = document.querySelector("#playButton");
-
-function MediaPlayer(config) {
-	this.media = config.el;
-}
-
-MediaPlayer.prototype.play = function () {
-	this.media.play();
-};
-
 const player = new MediaPlayer({ el: video });
 
-// HTMLMediaElement - Web API reference MDN
-button.onclick = () => player.play();
+const button = document.querySelector("#playButton");
+button.onclick = () => player.togglePlay();
